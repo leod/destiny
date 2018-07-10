ceb11c660598cf998199cf5512e440e4da386af6
Leonard Dahlmann <leo.dahlmann@gmail.com>
2018-07-10 22:37:45 +0200

    Add some traits for lockstep

--- lockstep/src/lib.rs
+++ lockstep/src/lib.rs
@@ -1,7 +1,23 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+extern crate serde;
+
+use std::fmt::Debug;
+use std::hash::Hash;
+
+use serde::{Serialize, Deserialize};
+
+pub struct PlayerId(u32);
+
+pub trait Command: Eq + PartialEq + Clone + Debug + Deserialize<'static> + Serialize
+{
+}
+
+pub struct TurnCommands<Command: self::Command>(Vec<(PlayerId, Vec<Command>)>);
+
+pub trait State: Eq + PartialEq + Hash + Clone + Debug + Deserialize<'static> + Serialize
+{
+    type Command: self::Command;
+
+    fn is_possible(&self, command: &Self::Command) -> bool;
+
+    fn execute(&mut self, commands: &TurnCommands<Self::Command>);
 }
